e6a57a71b6753ef444d96847875e2b8d9687dd43
ArteCervantes <artemisa.cervantes@Internet.local>
2015-06-25 16:41:37 -0500

    sitiogq
    sitio

---
+++ paginas/contacto.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width; initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="../css/demo.css" />
+<link rel="stylesheet" type="text/css" href="../css/icons.css" />
+<link rel="stylesheet" type="text/css" href="../css/component.css" />
+<link href='http://fonts.googleapis.com/css?family=Lato:100,400' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Oxygen:400,300,700' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Lora:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+<!--[if lt IE 7.]>
+<script defer type="text/javascript" src="../js/pngfix.js"></script>
+<![endif]-->
+<!--[if lt IE 9]>
+	<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+<![endif]-->
+<title>Formatos Publicitarios</title>
+<script src="../js/modernizr.custom.js"></script>
+</head>
+<body>
+<div class="container"> 
+  <!-- Push Wrapper --> 
+  <div class="mp-pusher" id="mp-pusher"> 
+  
+  
+  
+  <!-- mp-menu -->
+  <div id="mp-menu" class="mp-menu">
+    <div class="mp-level">
+    <div> <a href="../index.html"> <p>[X]</p> </a> </div>
+      <h2 style="text-align:left; padding-left:.9em;">Formatos Publicitarios</h2>
+      <ul>
+        <li><a class="" href="hometakeover.html">Home Take Over</a></li>
+        <li><a class="" href="patrociniodeseccion.html">Patrocinio Sección</a></li>
+        <li><a class="" href="dobleformato.html">Doble Formato</a></li>
+        <li><a class="" href="superespecial.html">Super Pushdown</a></li>
+        <li><a class="" href="newsletter.html">NewsLetter</a></li>
+        <li><a class="" href="mailing.html">Email</a></li>
+        <li class="icon icon-arrow-right-3"> <a class="ico" href="#"> Tradicionales</a>
+          <div class="mp-level">
+            <h2 class="">Tradicionales</h2>
+            <a class="mp-back" href="#">back</a>
+            <ul>
+                <li class="icon icon-arrow-right-3"> <a class="" href="box.html">Box Banner</a> </li>
+                <li class="icon icon-arrow-right-3"> <a class="" href="sky.html">Skyscraper</a> </li>
+                <li class="icon icon-arrow-right-3"> <a class="" href="super.html">Super Banner</a> </li>
+              </ul>
+          </div>
+        </li>
+       
+        <li><a class="icon icon-mail" href="contacto.php">Contacto</a> <br />
+          <p> </p>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <!-- /mp-menu -->
+  <div class="scroller"> 
+    <!-- this is for emulating position fixed of the nav -->
+    <div class="scroller-inner">
+      <div class="header">
+          <div> <a href="#" id="trigger" class="menu-trigger"> <span style="margin-top:2px;" class="bar"></span> <p> MENU</p><span class="bar"></span></a> </div>
+          <div id="logo"><a href="../index.html"><img src="../img/gq.png" width="161" height="41" /></a></div>
+        <ul class="sitios">
+          <li>Más sitios de <span class="conde-nast">CONDÉ NAST DIGITAL</span> México</li>
+          <li><a href="#" class="AD"><span>AD</span></a></li>
+          <li><a href="#" class="Glamour"><span>Glamour</span></a></li>
+          <li><a href="#" class="VF"><span>VF</span></a></li>
+          <li><a href="#" class="Vogue"><span>Vogue</span></a></li>
+        </ul>
+
+        </div>
+    </div>
+    <!-- /scroller-inner -->
+    <div class ="tituloimagenes">
+      <p> Contacto </p>
+    </div>
+    <!-- contenido-->
+    
+    <div class="Imagen"> <img src="../img/sombrin.jpg" /> </div>
+    <div class ="Contacto" style="line-height:200%; margin-top:2em; width: 477px; margin: 0px auto 0px auto; "> 
+      
+      <!-- Form -->
+      <form action="#" id="form" method="post" name="form" size="32em" style="line-height:175%; width: 477px; margin: 0px auto 0px auto; text-align:left;">
+        <br />
+        Nombre:
+        <input name="vname" placeholder="" type="text" value="" size="32em" style="margin-left:28px;">
+        <br />
+        Email:
+        <input name="vemail" placeholder="" type="text" value="" size="32em" style="margin-left:45px;">
+        <br />
+        Asunto:
+        <input name="sub" placeholder="" type="text" value="" size="32em" style="margin-left:36px;">
+        <br />
+        <textarea name="msg" rows="15" placeholder="Escribe aquí..." style="width: 477px;
+  height: 201px; margin-top:15px;"></textarea>
+        <br/>
+        <br />
+        <input id="send" name="submit" type="submit" value="Enviar" style="margin-left:214.5px;" >
+      </form>
+      <h3>
+        <?php include "secure_email_code.php"?>
+      </h3>
+      <table align="center" width="500" border="0" cellspacing="0" cellpadding="0" margin-top="0px";
+	margin-bottom="0px">
+        <tr>
+          <td><h4 style="margin-top: 0px; margin-bottom: 0px;" text-align:left;>Director Comercial Digital</h4></td>
+          <td><h4 style="margin-top: 0px; margin-bottom: 0px;" text-align:left;>Comercial Digital</h4></td>
+        </tr>
+        <tr>
+          <td><p style="margin-top: 0px; margin-bottom: .5em;">LUIS OREZZA</p></td>
+          <td><p style="margin-top: 0px; margin-bottom: .5em;">PAOLA ABAD</p></td>
+        </tr>
+        <tr>
+          <td><p class="icon icon-phone" style="margin-top: 0px; margin-bottom: 0px; text-align:left;"> +52 55 50623796</p></td>
+          <td><p class="icon icon-phone" style="margin-top: 0px; margin-bottom: 0px; text-align:left;"> +52 55 50623763</p></td>
+        </tr>
+        <tr>
+          <td><p class="icon icon-mail" style="margin-top: 0px; margin-bottom: 0px; text-align:left;"><a style="color:#333" href="mailto:luis.orezza@condenast.com.mx"> luis.orezza@condenast.com.mx</a></p></td>
+          <td><p class="icon icon-mail" style="margin-top: 0px; margin-bottom: 0px; text-align:left;"><a style="color:#333" href="mailto:paola.abad@condenast.com.mx"> paola.abad@condenast.com.mx</a></p></td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</div>
+</div>
+<!-- /scroller -->
+</div>
+
+<!-- /pusher -->
+</div>
+<!-- /container --> 
+<script src="../js/classie.js"></script> 
+<script src="../js/mlpushmenu.js"></script> 
+<script>
+			new mlPushMenu( document.getElementById( 'mp-menu' ), document.getElementById( 'trigger' ), {
+				type : 'cover'
+			} );
+			
+			
+</script>
+</body>
+</html>
+
+<!--
+			
+			function validarDatosPOST() {
+				var verificar = true;
+				if(!document.valida_datos_post_frm.Nombre.value){
+					alert("El campo de Nombre es requerido");
+					document.valida_datos_post_frm.Nombre.focus();
+					verificar = false;
+				}
+				
+				else if(!document.valida_post_frm.Apellido.value){
+					alert("El campo de Apellido es requerido");
+					document.valida_datos_post_frm.Apellido.focus();
+					verificar = false;
+				}
+				
+				else if(!document.valida_datos_post_frm.Mail.value){
+					alert("El campo de Mail es requerido");
+					document.valida_datos_post_frm.Mail.focus();
+					verificar = false;
+				}
+				
+				else if(!document.valida_datos_post_frm.Asunto.value){
+					alert("El campo de Asunto es requerido");
+					document.valida_datos_post_frm.Asunto.focus();
+					verificar = false;
+				}
+			if (verificar) {
+				document.valida_datos_post_frm.submit();
+				}	
+			}
+			
+			window.onload = function(){
+				document.getElementById("enviar-post").onclick = validarDatosPOST;
+				}
+			-->
